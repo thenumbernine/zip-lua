14b96a10e5c6a093afabbfb3440427d320152686
thenumbernine <christopher.e.moore@gmail.com>
2024-06-05 20:15:27 +0800

    added tar also ... idk maybe I'll move tar to its own project, maybe I'll rename this to 'archive', idk

---
+++ tar.lua
@@ -0,0 +1,66 @@
+--[[
+I could make a separate 'tar' project
+but it's probably a better idea to just put this all in an 'archive' project
+and abstract the interface for various formats
+--]]
+local ffi = require 'ffi'
+local class = require 'ext.class'
+local GCWrapper = require 'ffi.gcwrapper.gcwrapper'
+local archive = require 'ffi.req' 'archive'
+
+local TarArchive = class(GCWrapper{
+	gctype = 'autorelease_tar_t',
+	ctype = 'struct archive*',
+	release = function(ptr)
+		if ptr[0] ~= nil then
+			archive.archive_read_free(ptr[0])
+			ptr[0] = nil
+		end
+	end,
+})
+
+local function archiveAssert(fname, handle, ...)
+	local ret = archive[fname](handle, ...)
+	if ret == archive.ARCHIVE_EOF 
+	or ret == archive.ARCHIVE_OK
+	then
+	elseif ret == archive.ARCHIVE_WARN then
+		-- does ARCHIVE_WARN set archive_error_string() ? can't find this in the docs ... 
+		io.stderr:write(fname..' warning: '..ffi.string(archive.archive_error_string(handle))..'\n')
+	else--if ret == archive.ARCHIVE_RETRY
+		-- are there unlisted error codess? example codes hint at this , using < instead of == for ok vs warn vs fatal
+		error(fname..' failed with '..ffi.string(archive.archive_error_string(handle)))
+	end
+	return ret
+end
+
+function TarArchive:init(fn)
+	TarArchive.super.init(self)
+	self.archive = archive.archive_read_new()
+	if self.archive == nil then error'archive_read_new failed' end
+	self.gc.ptr[0] = self.handle
+
+	archiveAssert('archive_read_support_format_all', self.handle)
+	archiveAssert('archive_read_open_filename', self.handle, filename, 10240)
+end
+
+function TarArchive:dir()
+	local entry = ffi.new'struct archive_entry[1]'
+	while true do
+		local ret = archive.archive_read_next_header(self.handle, entry)
+		if ret == archive.ARCHIVE_EOF then 
+			break 
+		end
+		--local size = archive.archive_entry_size(entry)
+		
+		-- coroutine.yield(TarPath(self, entry))
+	end
+	
+	archive.archive_read_close(self.handle)
+end
+
+function TarArchive:file(fn)
+	error'TODO'
+end
+
+return TarArchive 
